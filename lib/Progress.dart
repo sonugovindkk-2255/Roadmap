595e7cb910c932765c713aa30d15e3d928c75c03
Sonugovind K K <sonugovind.kk@pitsolutions.com>
2026-01-21 15:33:35 +0530

    update master with new changes

--- lib/Progress.dart
+++ lib/Progress.dart
@@ -1,148 +1,39 @@
 
-
 import 'package:flutter/material.dart';
 
-class Progress extends StatefulWidget {
-  const Progress({super.key});
+class BlobBadge extends StatelessWidget {
+  const BlobBadge({super.key});
 
-  @override
-  State<Progress> createState() => _ProgressState();
-}
-
-class _ProgressState extends State<Progress> {
-
-  static const Size worldSize = Size(360, 1000);
-  static const int segmentsCount = 4;
-  static const double segmentHeight = 200;
-
-  late final List<CubicSegment> segments =
-  generateSnakePath(screenWidth: worldSize.width, segmentHeight: segmentHeight, segments: segmentsCount);
-  final List<double>milestoneTs =[0,0.23,0.48,0.64,0.85];
-  final currentIndex = 1;
 
   @override
   Widget build(BuildContext context) {
-    const Size wSize = worldSize;
-    return Scaffold(
-      backgroundColor:Colors.lime[100],
-      appBar: AppBar(
-        title: Text('Progress',style: TextStyle(
-          color: Colors.orangeAccent
-        ),),
-        centerTitle: true,
-      ),
-      body: LayoutBuilder(
-          builder: (context,viewport){
-            final scale = viewport.maxWidth / wSize.width;
-            final canvasSize = Size(viewport.maxWidth,wSize.height*scale);
-            return SingleChildScrollView(
-              physics:const BouncingScrollPhysics(),
-              child: SizedBox(
-                height: canvasSize.height,
-                width: canvasSize.width,
-                child: Stack(
-                  children: [
-                    CustomPaint(
-                      size: canvasSize,
-                      painter: RoutePainter(
-                          segments: segments,
-                          scale: scale,
-                          bandwidth: 36,
-                          activeColor: Color(0xFF4CAF50),
-                          restColor:Color(0xFFFFE0B2),
-                          progressT: milestoneTs[(currentIndex).clamp(0, milestoneTs.length -1)],
-                    ))
-                  ],
-
-                ),
-              ),
-            );
-          }),
+    return CustomPaint(
+      painter: _blobPainter(),
+      size:const Size(120,120),
     );
   }
 }
-class CubicSegment{
-  const CubicSegment(this.p0,this.p1,this.p2,this.p3);
-  final p0,p1,p2,p3;
-}
-List<CubicSegment> generateSnakePath({
-  required double screenWidth,required double segmentHeight,int segments =4,
-}) {
-  final List<CubicSegment> path = [];
-  double y = 0;
-  bool goRight = true;
-  for (int i = 0; i < segments; i++) {
-    if (goRight) {
-      path.add(CubicSegment(Offset(0, y),
-          Offset(screenWidth * .33, y),
-          Offset(screenWidth * .66, y + segmentHeight),
-          Offset(screenWidth, y + segmentHeight)
-      ),);
-    } else {
-      path.add(
-        CubicSegment(
-          Offset(screenWidth, y),
-          Offset(screenWidth * 0.66, y),
-          Offset(screenWidth * 0.33, y + segmentHeight),
-          Offset(0, y + segmentHeight),
-        ),
-      );
-    }
-    y += segmentHeight;
-    goRight = !goRight;
-  }
-  return path;
-}
-
-
-class RoutePainter extends CustomPainter{
-  RoutePainter({required this.segments,required this.scale,required this.bandwidth,
-  required this.activeColor,required this.restColor,required this.progressT});
-  final List<CubicSegment> segments;
-  final double scale;
-  final double bandwidth;
-  final Color activeColor;
-  final Color restColor;
-  final double progressT;
+class _blobPainter extends CustomPainter{
   @override
-  void paint( Canvas canvas, Size size){
-    final worldPath = Path();
-    var started = false;
-    for(final s in segments){
-      if (!started){
-        worldPath.moveTo(s.p0.dx, s.p0.dy);
-        started = true;
-      }
-      worldPath.cubicTo(s.p1.dx,s.p1.dy,s.p2.dx,s.p2.dy,s.p3.dx,s.p3.dy);
-    }
-    final path = worldPath.transform((Matrix4.diagonal3Values(scale,scale,1)).storage);
-    final basePaint = Paint()
-      ..color = restColor
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = bandwidth;
-    final activePaint = Paint()
-      ..color = activeColor
-      ..style = PaintingStyle.stroke
-      ..strokeCap = StrokeCap.round
-      ..strokeWidth = bandwidth;
-    canvas.drawPath(path, basePaint);
-    if (progressT> 0){
-      final metric = path.computeMetrics().isEmpty ? null:path.computeMetrics().first;
-      if (metric != null){
-        final len =(metric.length * progressT.clamp(0,1));
-        final activePath = metric.extractPath(0, len);
-        canvas.drawPath(activePath, activePaint);
-      }
-    }
+  void paint(Canvas canvas, Size size){
+    final w =size.width;
+    final h =size.height;
+    const lightGreen = Color(0xFFBDD070);
+    const darkGreen  = Color(0xFF5B740E);
+    final leaf = Path();
+    final fillLeaf = Paint()
+      ..color = darkGreen
+      ..style = PaintingStyle.fill;
+    leaf.moveTo(w * 0.59, h * 0.75);
+    leaf.cubicTo(w * 0.53, h * 0.55, w * 0.64, h * 0.43, w * 0.59, h * 0.28);
+    leaf.cubicTo(w * 0.49, h * 0.39, w * 0.47, h * 0.51, w * 0.46, h * 0.74);
+    leaf.close();
+    canvas.drawPath(leaf,fillLeaf);
+    canvas.save();
+    canvas.translate(10,5);
+    canvas.restore();
   }
   @override
-  bool shouldRepaint(covariant RoutePainter old){
-    return old.segments!= segments||
-    old.scale != scale||
-    old.bandwidth != bandwidth||
-    old.activeColor !=activeColor||
-    old.restColor != restColor||
-    old.progressT != progressT;
-  }
+  bool shouldRepaint(covariant CustomPainter oldDelegate)=> false;
+
 }
--- lib/pro.dart
+++ lib/pro.dart
@@ -1,6 +1,7 @@
 
 
 import 'package:flutter/material.dart';
+import 'package:project_2/Progress.dart';
 
 class Roadmap extends StatefulWidget {
   const Roadmap({super.key});
@@ -10,17 +11,31 @@ class Roadmap extends StatefulWidget {
 }
 
 class _RoadmapState extends State<Roadmap> {
+  int currentProgress = 1;
   @override
   Widget build(BuildContext context) {
-    final int steps = 20;
+    final int steps = 100;
+    final Map<int,String> leftAssets= {
+     0: 'Asset/images/Vector.png',
+     1: 'Asset/images/image1.png',
+      2:'Asset/images/Frame 2085664030.png'
+    };
+    final Map<int,String> rightAssets= {
+      0: 'Asset/images/Frame 2085664032.png',
+      1: 'Asset/images/Group.png',
+      2:'Asset/images/Frame 2085664032.png'
+    };
+    String _getLeftAsset(int i) => leftAssets[i] ??((i%2==0) ?'Asset/images/Group.png':'Asset/images/Frame 2085664032.png');
+    String _getRightAsset(int i) => rightAssets[i] ?? ((i%2==0)?'Asset/images/image1.png':'Asset/images/Group.png');
+
     return Scaffold(
 
-      backgroundColor: Colors.amber[100],
+      backgroundColor: Colors.green[50],
       body:LayoutBuilder(
             builder:(context,constraints){
               double w =constraints.maxWidth;
               double h = constraints.maxHeight;
-              double stepHeight = 200.0;
+              double stepHeight = 250.0;
               double totalHeight = steps * stepHeight;
 
               List<Offset> nodes =List.generate(steps,(i){
@@ -32,15 +47,48 @@ class _RoadmapState extends State<Roadmap> {
                   height: totalHeight,
                   child: Stack(
                     children: [
+                      ...nodes.asMap().entries.map((entry){
+                        int i = entry.key;
+                        if(i>=nodes.length-1) return const SizedBox();
+                        double midY =(nodes[i].dy+nodes[i+1].dy)/2;
+                        double screenWidth =MediaQuery.of(context).size.width;
+                        return Stack(
+                          children: [
+                            Positioned(left:0,
+                                top: midY-60,
+                                child: Image.asset(_getLeftAsset(i))),
+                            Positioned(right:5,
+                                top: midY-40,
+                                child: Image.asset(_getRightAsset(i)))
+                         ,
+                          ],
+                        ); 
+
+                      }),
+                      Padding(padding:EdgeInsetsGeometry.symmetric(horizontal: 32,vertical: 192),
+                      child: Image.asset('Asset/images/image2.png'),),
+                      Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 144,vertical: 240),
+                      child: Image.asset('Asset/images/Vector.png'),),
+                      Padding(padding:EdgeInsetsGeometry.symmetric(horizontal: 180,vertical: 272),
+                      child: Image.asset('Asset/images/image3.png'),),
+                      Padding(padding: EdgeInsetsGeometry.symmetric(horizontal: 180,vertical: 480),
+                      child: Image.asset('Asset/images/image2.png'),),
                       CustomPaint(
                         size: Size(w,totalHeight),
-                        painter: RoadPainter(nodes: nodes),
+                        painter: RoadPainter(nodes: nodes, progress: currentProgress),
                       ),
                       ...nodes.asMap().entries.map((entry){
+                        int index = entry.key;
+                        bool isVisited = index<currentProgress;
                         return Positioned(
                           left: entry.value.dx-30,
                           top:entry.value.dy-30 ,
-                            child: _buildNode(entry.key),);
+                            child:GestureDetector(
+                              onTap: (){
+                                setState(()=> currentProgress = index+1);
+                              },
+                              child: _buildNode(index, isVisited),
+                            ));
                       }).toList(),
                     ],
                   ),
@@ -51,60 +99,84 @@ class _RoadmapState extends State<Roadmap> {
     );
   }
 
-  Widget _buildNode(int index){
+  Widget _buildNode(int index,bool isVisited){
     return Container(
       width: 60,
       height: 60,
       decoration: BoxDecoration(
-          color:Colors.white,
+          color:isVisited ? Colors.orange:Colors.white,
           shape: BoxShape.circle,
           border: Border.all(color: Colors.green,width: 3),
           boxShadow:[BoxShadow(blurRadius: 4,color: Colors.black26)]
       ),
-      child: Image.asset(index%2 == 0? "Asset/images/Screenshot_19-1-2026_10403_www.bing.com.jpeg":"Asset/images/cartoon-cute-school-boy-photo.jpg")
+      child: Image.asset(index%2 == 0? "Asset/images/Group (1).png":"Asset/images/Frame 2085664036.png")
     );
   }
-
-  Widget _buildLake(double screenWidth){
-    return Container(
-      width: screenWidth*0.4,
-      height: 100,
-      decoration: BoxDecoration(
-        color: Colors.blue[200],
-        borderRadius: BorderRadius.only(topLeft: Radius.circular(50),
-        bottomLeft: Radius.circular(50))
-      ),
-    );
+  Widget _buildBlob(double w,double h){
+    return Positioned(
+      top: h*.45,
+      right: 10,
+        child:Image.asset('Asset/images/Group.png'));
   }
 }
 class RoadPainter extends CustomPainter{
-  RoadPainter({required this.nodes});
+  RoadPainter({required this.nodes,required this.progress});
   final List<Offset> nodes;
+  final int progress;
   @override
   void paint(Canvas canvas, Size size) {
     if (nodes.length < 2) return;
     final backgroundPaint = Paint()
-      ..color = Color(0xFFFFCC80)
+      ..color = Color(0xFFFFE0B2)
       ..style = PaintingStyle.stroke
       ..strokeWidth = 40
       ..strokeCap = StrokeCap.round;
-    // final activePaint = Paint()
-    //   ..color = Color(0xFF4CAF50)
-    //   ..style = PaintingStyle.stroke
-    //   ..strokeWidth = 40
-    //   ..strokeCap = StrokeCap.round;
-    final path = Path();
-    double w = size.width;
-    double h = size.height;
-    path.moveTo(nodes[0].dx, nodes[0].dy);
-    for (int i = 0; i < nodes.length - 1; i++) {
-      Offset start = nodes[i];
-      Offset end = nodes[i + 1];
-      double controlY = (start.dy + end.dy)/2;
-      path.cubicTo(start.dx, controlY, end.dx, controlY, end.dx, end.dy);
+     final activePaint = Paint()
+      ..color = Color(0xFF4CAF50)
+      ..style = PaintingStyle.stroke
+      ..strokeWidth = 40
+     ..strokeCap = StrokeCap.round;
+    Path fullpath = Path();
+    fullpath.moveTo(nodes[0].dx,nodes[0].dy);
+    for(int i =0;i<nodes.length-1;i++){
+      _addSBeizer(fullpath, nodes[i],nodes[i+1],size.width);
     }
-    canvas.drawPath(path, backgroundPaint);
+    canvas.drawPath(fullpath, backgroundPaint);
+    Path activepath = Path();
+    activepath.moveTo(nodes[0].dx,nodes[0].dy);
+    for(int i =0;i<progress-1;i++){
+      _addSBeizer(activepath, nodes[i], nodes[i+1],size.width);
+    }
+    canvas.drawPath(activepath,activePaint);
+
+
+    }
+    void _addSBeizer(Path path,Offset p1,Offset p2,double screenWidth){
+
+
+    double bendIntensity = (p1.dx<p2.dx) ? screenWidth *0.88:screenWidth*0.12;
+      double controlY =(p1.dy+p2.dy)/2;
+      path.cubicTo(bendIntensity,p1.dy,
+                   bendIntensity,controlY,
+                   bendIntensity,controlY);
+      path.cubicTo(bendIntensity,controlY,
+                   bendIntensity,p2.dy,
+                    p2.dx,p2.dy);
+
   }
+
+
+
+
+
   @override
-  bool shouldRepaint( CustomPainter oldDelegate)=>true;
+  bool shouldRepaint(RoadPainter oldDelegate)=>oldDelegate.progress != progress;
+
+
 }
+
+
+
+
+
+
